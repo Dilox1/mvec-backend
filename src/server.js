7ab7adb8d2f0cb2ad533bcbe0314ceca23ae2faa
Bikmandjuma <bikmangeek@gmail.com>
2026-09-11 12:11:01 +0200

    first commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "https://mvec.up.railway.app",
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
+app.use(express.json());
+
+// routes
