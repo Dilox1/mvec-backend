b03944aa73814322dcb84f40f9bbf47653302fa0
Bikmandjuma <bikmangeek@gmail.com>
2026-09-13 10:56:02 +0200

    Fix CORS

--- server.js
+++ server.js
@@ -25,7 +25,13 @@ app.use(
     credentials: true,
   }),
 );
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({
+  limit: "50mb",
+  extended: true,
+}));
+
 app.use(morgan("dev"));
 
 app.use("/api/auth", require("./src/routes/auth.routes"));
