03fcd288314956f5c8d9b1db974e799c518c4eaa
Bikmandjuma <bikmangeek@gmail.com>
2026-09-13 10:23:47 +0200

    Fix CORS

--- src/server.js
+++ src/server.js
@@ -1,3 +1,23 @@
+// const express = require("express");
+// const cors = require("cors");
+
+// const app = express();
+
+// const corsOptions = {
+//   origin: [
+//     "http://localhost:5173",
+//     "https://mvec.up.railway.app",
+//   ],
+//   credentials: true,
+//   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+// };
+
+// app.use(cors(corsOptions));
+// app.options("*", cors(corsOptions));
+
+// app.use(express.json());
+
 const express = require("express");
 const cors = require("cors");
 
@@ -16,4 +36,11 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
 
-app.use(express.json());
+// Increase request body limits
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({
+  limit: "50mb",
+  extended: true,
+}));
+
+// Your routes below this point
